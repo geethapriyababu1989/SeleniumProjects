7d770efa910f281f320a7f8d9b685e476b537758
my <geethabpriya89@gmail.com>
2020-07-02 18:15:28 +0530

    committing after edit

--- TestNGProject/src/test/java/examples/ASampleClassForGit.java
+++ TestNGProject/src/test/java/examples/ASampleClassForGit.java
@@ -5,6 +5,7 @@ public class ASampleClassForGit {
 	public static void main(String[] args) {
 System.out.println("Git push");
 System.out.println("Git push");
+System.out.println("Git push");
 	}
 
 }
